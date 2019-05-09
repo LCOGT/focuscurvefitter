8d19252da6a63e0aacc14818a23ff93be5d69d2f
Daniel Harbeck <drharbeck@gmail.com>
2019-05-09 15:48:46 -0700

    code cleanup

--- lcofocuscurvefit/curvefit.py
+++ lcofocuscurvefit/curvefit.py
@@ -1,17 +1,13 @@
 import argparse
-import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
-import logging
 import json
 
-_logger = logging.getLogger(__name__)
-
+# This describes our model for a focus curve: Seeing and defocus add in quadrature.
 sqrtfit = lambda x, seeing, bestfocus, slope: (seeing ** 2 + (slope * (x - bestfocus)) ** 2) ** 0.5
 
-
-def dofit(xdata, ydata, func=sqrtfit, plot=False):
+def focus_curve_fit(xdata, ydata, func=sqrtfit, plot=False):
     # TODO: Verification that we have enough points.
 
     for iter in range(2):
@@ -23,9 +19,8 @@ def dofit(xdata, ydata, func=sqrtfit, plot=False):
         elif len(paramset) == 2:
             fit = func(xdata, paramset[0], paramset[1])
         else:
-            _logger.error("Order of fit not supported")
             return None
-
+        # sigma rejection and rms estimate.
         delta = ydata - fit
         s = np.std(delta)
         good = delta < 2 * s
@@ -41,21 +36,25 @@ def dofit(xdata, ydata, func=sqrtfit, plot=False):
 
 
 def parseCommandLine():
+
     parser = argparse.ArgumentParser(
         description='Fit a focus curve to an autofocus sequence result.')
-
     parser.add_argument("--focuslist", nargs='+', type=float)
     parser.add_argument("--fwhmlist", nargs='+', type=float)
-    parser.add_argument('--plotit', action='store_true',
+    parser.add_argument('--makepng', action='store_true',
                         help='make a nice plot')
-    parser.add_argument("--plotname", default="focusplot.png", help="Name of output plot file")
+    parser.add_argument("--pngname", default="focusplot.png", help="Name of output plot file")
 
     args = parser.parse_args()
+
     if (len(args.focuslist) != len(args.fwhmlist)) or (len(args.focuslist) < 4):
-        _logger.error(
-            "Invalid input: either the input liskts differ in size or do not have enough entries (minimum is 4:\n{}\n{}".format(
-                args.focuslist, args.fwhmlist))
-        exit(1)
+
+        # TODO: For service integration, trickle up error condition.
+        print(json.dumps({'fitok': False,
+                          'errormsg': "Invalid input: either the input liskts differ in size or do not have enough entries (minimum is 4:\n{}\n{}".format(
+                              args.focuslist, args.fwhmlist)}))
+        exit(0)
+
     args.focuslist = np.asarray(args.focuslist)
     args.fwhmlist = np.asarray(args.fwhmlist)
     return args
@@ -64,7 +63,7 @@ def parseCommandLine():
 def main():
     args = parseCommandLine()
 
-    if args.plotit:
+    if args.makepng:
         plt.figure()
         plt.plot(args.focuslist, args.fwhmlist, 'o')
         plt.xlabel("FOCUS Demand [mm foc plane]")
@@ -72,13 +71,13 @@ def main():
         plt.xlim([-3.6, 3.6])
         plt.ylim([0, 14])
 
-    p, rms = dofit(args.focuslist, args.fwhmlist, sqrtfit, plot=args.plotit)
+    p, rms = focus_curve_fit(args.focuslist, args.fwhmlist, sqrtfit, plot=args.makepng)
 
-    if args.plotit:
+    if args.makepng:
         plt.axvline(x=p[1], label="best focus sqrt")
         plt.legend()
         plt.title("Sqrt best focus found at {:5.2f}".format(p[1]))
-        plt.savefig("focus_{}.png".format(jackstart))
+        plt.savefig("{}".format(args.pngname))
 
     returnpackage = {'fitok': True,
                      'fit_seeing': p[0],
@@ -86,7 +85,9 @@ def main():
                      'fit_slope': p[2],
                      'fit_rms': rms}
 
+    #TODO: Eventually return json from a web query. So far, we dump stdout.
     print(json.dumps(returnpackage))
+
 
 if __name__ == '__main__':
     main()
