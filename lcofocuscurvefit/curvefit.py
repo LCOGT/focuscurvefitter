57b2ebd8477eb8d8bdd8eb5c7c3a6d7a027b247a
Daniel Harbeck <drharbeck@gmail.com>
2019-05-10 16:00:59 -0700

    extensive testing and more test cases

--- lcofocuscurvefit/curvefit.py
+++ lcofocuscurvefit/curvefit.py
@@ -1,4 +1,6 @@
 import argparse
+
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
@@ -6,37 +8,33 @@ import json
 
 # This describes our model for a focus curve: Seeing and defocus add in quadrature.
 sqrtfit = lambda x, seeing, bestfocus, slope: (seeing ** 2 + (slope * (x - bestfocus)) ** 2) ** 0.5
+polyfit = lambda x, p0, p1, p2: p2 * (x-p1)**2 + p0
+
 
 def focus_curve_fit(xdata, ydata, func=sqrtfit, plot=False):
     # TODO: Verification that we have enough points.
 
     for iter in range(2):
         (paramset, istat) = optimize.curve_fit(func, xdata, ydata)
-        # TODO: Verification that fit succeeded
-
-        if len(paramset) == 3:
-            fit = func(xdata, paramset[0], paramset[1], paramset[2])
-        elif len(paramset) == 2:
-            fit = func(xdata, paramset[0], paramset[1])
-        else:
-            return None
-        # sigma rejection and rms estimate.
+        # sigma rejection and rms estimate:
+        fit = func(xdata, *paramset)
         delta = ydata - fit
         s = np.std(delta)
-        good = delta < 2 * s
+        good = (delta < 3 * s)
         xdata = xdata[good]
         ydata = ydata[good]
 
+    paramerrors = np.sqrt(np.diag(istat))
+
     if plot:
         base = np.arange(-3.6, 3.6, 0.1)
-        y = func(base, paramset[0], paramset[1], paramset[2])
-        plt.plot(base, y, "--")
+        y = func(base, *paramset)
+        plt.plot(base, y, "--", label="sqrt" if func == sqrtfit else "parabola")
 
-    return paramset, s
+    return paramset, paramerrors
 
 
 def parseCommandLine():
-
     parser = argparse.ArgumentParser(
         description='Fit a focus curve to an autofocus sequence result.')
     parser.add_argument("--focuslist", nargs='+', type=float)
@@ -48,7 +46,6 @@ def parseCommandLine():
     args = parser.parse_args()
 
     if (len(args.focuslist) != len(args.fwhmlist)) or (len(args.focuslist) < 4):
-
         # TODO: For service integration, trickle up error condition.
         print(json.dumps({'fitok': False,
                           'errormsg': "Invalid input: either the input liskts differ in size or do not have enough entries (minimum is 4:\n{}\n{}".format(
@@ -58,6 +55,10 @@ def parseCommandLine():
     args.focuslist = np.asarray(args.focuslist)
     args.fwhmlist = np.asarray(args.fwhmlist)
     return args
+
+def makeprettyplot():
+    #TODO: Move all the plotting stuff in here
+    pass
 
 
 def main():
@@ -69,24 +70,29 @@ def main():
         plt.xlabel("FOCUS Demand [mm foc plane]")
         plt.ylabel("FWHM (Pixels")
         plt.xlim([-3.6, 3.6])
-        plt.ylim([0, 14])
+        plt.ylim([0, 20])
 
+    focus_curve_fit(args.focuslist, args.fwhmlist, polyfit, plot=args.makepng)
     p, rms = focus_curve_fit(args.focuslist, args.fwhmlist, sqrtfit, plot=args.makepng)
 
+    deltafocus = rms[1]
     if args.makepng:
-        plt.axvline(x=p[1], label="best focus sqrt")
+        if math.isfinite(deltafocus):
+            plt.axvline(x=p[1], label="best focus sqrt")
         plt.legend()
-        plt.title("Sqrt best focus found at {:5.2f}".format(p[1]))
+        plt.title("Sqrt best focus found at {:5.2f} +/- {:5.2f}".format(p[1], deltafocus) if math.isfinite(
+            deltafocus) else "Fit failed")
         plt.savefig("{}".format(args.pngname))
 
-    returnpackage = {'fitok': True,
+    returnpackage = {'fitok': True if math.isfinite(deltafocus) else False,
                      'fit_seeing': p[0],
                      'fit_focus': p[1],
                      'fit_slope': p[2],
-                     'fit_rms': rms}
+                     'fit_rms': deltafocus}
 
-    #TODO: Eventually return json from a web query. So far, we dump stdout.
+    # TODO: Eventually return json from a web query. So far, we dump to stdout.
     print(json.dumps(returnpackage))
+    return returnpackage
 
 
 if __name__ == '__main__':
--- lcofocuscurvefit/tests/test_curvefit.py
+++ lcofocuscurvefit/tests/test_curvefit.py
@@ -1,14 +1,41 @@
 import subprocess as subp
 import json
+from delayed_assert import expect, assert_expectations
 
 testset = [{'focuslist': "-2.0 -1.5 -1.0 -0.5 0.0 0.5 1.0 1.5 2.0",
             'fwhmlist': "3.6 3.57 3.74 4.21 5.34 5.89 7.1 8.26 9.46",
             'expect_fitok': True,
             'expect_bestfocus': -1.53},
+
+           {'focuslist': " 2.0 1.5 1.0 0.5 0.0 -0.5 -1.0 -1.5 -2.0",
+            'fwhmlist': "8.1301 5.88946 3.55935 2.20952 1.33816 1.74272 3.36874 5.66773 7.77222",
+            'expect_fitok': True,
+            'expect_bestfocus': 0.0},
+            #using mean values of largfe defocus
+           {'focuslist': " 2.0 1.5 1.0 0.5 0.0 -0.5 -1.0 -1.5 -2.0",
+            'fwhmlist': "17.7 16.2 14.8 10.4 7.7 5.5 3.3 2.0 1.6",
+            'expect_fitok': True,
+            'expect_bestfocus': -1.95},
+
+             #using median values fo large defocus
+           {'focuslist': " 2.0 1.5 1.0 0.5 0.0 -0.5 -1.0 -1.5 -2.0",
+            'fwhmlist': "19.67562 17.3883 14.77033 10.54968 7.65941 5.25539 3.15868 1.77384 1.39651",
+            'expect_fitok': True,
+            'expect_bestfocus': -1.95},
+
+           {'focuslist': "-3.5 -2.5 -1.5 -0.5 0.5 1.5 2.5 3.5",
+            'fwhmlist': "4.19 3.3 3.5 4.98 7.18 9.46 12.32 14.81",
+            'expect_fitok': True,
+            'expect_bestfocus': -2.17},
+
+
 ]
 
-def curvaturefit(testset):
+def curvaturefit(testset, iter= None):
+
     command = './venv/bin/python lcofocuscurvefit/curvefit.py --focuslist {} --fwhmlist {}'.format(testset['focuslist'], testset['fwhmlist'])
+    if iter is not None:
+        command = command + " --makepng --pngname focus_{}.png".format (iter)
     try:
         results = subp.check_output([x for x in command.split()])
     except:
@@ -17,9 +44,15 @@ def curvaturefit(testset):
     results = json.loads(results)
     assert results['fitok'] == testset['expect_fitok']
     if results['fitok']:
-        assert abs(results['fit_focus'] - testset['expect_bestfocus']) < 0.1
+        expect (abs(results['fit_focus'] - testset['expect_bestfocus']) < 0.1, "example Nr {}".format (iter))
 
 
 def test_curvaturefit():
+    iter = 1
     for test in testset:
-        curvaturefit(test)
+        curvaturefit(test, iter)
+        iter = iter + 1
+    assert_expectations()
+
+
+
